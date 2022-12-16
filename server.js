24b7f043baba752fb167b92d29ce2aad1c830675
Anvar571 <abdurashidovanvar52@gmail.com>
2022-12-16 20:16:02 +0500

    add simple schema

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const express = require("express");
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const app = express()
+const mongoose = require("mongoose")
 
 const port = process.env.PORT
+const dbUrl = process.env.DBURL
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
+
+mongoose.connect(dbUrl, {
+}, (err) => {
+    if (err) return console.log(err);
+    console.log("db connect successfull");
+})
 
 app.listen(port, () => {
     console.log(`listen on port ${port}`);
