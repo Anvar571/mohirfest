65f2c066ce9e83ee0aa4e4afe172b3c7214a2cf7
Anvar571 <abdurashidovanvar52@gmail.com>
2022-12-17 02:29:34 +0500

    register and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
+  const token = localStorage.getItem("firstToken")
   return (
-    <div className="App">
-      <div className="row">
-        <div className="col-md-3">
-          <Menu/>
-        </div>
-        <div className="col-md-9">
-          <h2>Qismlar</h2>
-        </div>
-      </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={token ? <Home/> : <Login/>}/>
+          <Route exact path="/register" element={<Register/>}/>
+        </Routes>
     </div>
+    </Router>
   );
 }
 
