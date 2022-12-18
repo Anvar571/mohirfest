77949ae31e467dc9cc0ff98a15f2d9984742e9f6
Anvar571 <abdurashidovanvar52@gmail.com>
2022-12-18 21:36:30 +0500

    responsive site fineshid

--- client/src/App.js
+++ client/src/App.js
@@ -12,15 +12,10 @@ import MaslahatlarPage from "./components/maslahatlar/MaslahatlarPage";
 import ProfilePage from "./components/profile/ProfilePage";
 import NotFoudPage from "./pages/notFoundPage";
 import Yonalish from "./components/topshiriq/Yo'nalishlar/Yonalishlar";
-<<<<<<< HEAD
-
-// server add
-=======
 import { useSelector } from "react-redux";
 import Alert from "./components/alert/Alert";
 import { useDispatch } from "react-redux";
 import { refresh_token } from "./redux/actions/authAction";
->>>>>>> 95f9e8802850a76545b7fa4b58e4296ea7b2bf79
 
 function App() {
   const { auth } = useSelector(state => state);
@@ -34,16 +29,6 @@ function App() {
     <Router>
       <Alert/>
       <div className="">
-<<<<<<< HEAD
-        <div className="row">
-          <div className="col-2 col-sm-2 col-md-3">
-            {token && <Menu />}
-          </div>
-          <div className="col-10 col-sm-10 col-md-9">
-            {token && <Header />}
-            <Routes>
-              <Route path="/" element={token ? <Home /> : <Register />} />
-=======
         <div className={`row ${auth.token ? "" : 'd-flex justify-content-center align-items-center'} `}>
           {
             auth.token &&
@@ -54,7 +39,6 @@ function App() {
             {auth.token && <Header />}
             <Routes>
               <Route path="/" element={auth.token ? <Home /> : <Login />} />
->>>>>>> 95f9e8802850a76545b7fa4b58e4296ea7b2bf79
               <Route exact path="/login" element={<Login />} />
               <Route exact path="/register" element={<Register />} />
 
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { useState } from 'react';
 import { BsSearch } from "react-icons/bs";
 import '../../styles/home.css';
-<<<<<<< HEAD
-import { Link } from "react-router-dom"
-=======
 import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
->>>>>>> 95f9e8802850a76545b7fa4b58e4296ea7b2bf79
 
 function WelcomeUser(props) {
      return <span>{props.name}</span>;
@@ -20,11 +16,7 @@ const Header = () => {
           <div>
                <div className="container d-flex my-4 justify-content-between border-bottom align-items-center pb-4">
                     <div className="d-flex flex-column d-none d-sm-block d-md-block">
-<<<<<<< HEAD
-                         <h4 className="fs-6 fs-sm-6 fs-md-3">Salom <WelcomeUser name={`${user.username}`} /></h4>
-=======
                          <h4 className="fs-6 fs-sm-6 fs-md-3">Salom <WelcomeUser name={`${auth.user.username}`} /></h4>
->>>>>>> 95f9e8802850a76545b7fa4b58e4296ea7b2bf79
                          <span className="fs-6 fs-sm-6 fs-md-5">
                               {dateState.toLocaleString('en-US', {
                                    hour: 'numeric',
@@ -50,14 +42,9 @@ const Header = () => {
                     </div>
                     <div>
                          <Link to="/profile" style={{ textDecoration: "none" }}>
-<<<<<<< HEAD
-                              <span className="profileName d-none d-sm-none d-md-inline" style={{ color: "#000", paddingRight: "15px" }}>{user.fullname}</span>
-                              <img src={UserAvatar} alt="User Avatar" />
-=======
                               <span className="profileName d-none d-sm-none d-md-inline" style={{ color: "#000", paddingRight: "15px" }}>{auth.user.fullname}</span>
                               <img src={auth.user.avatar} alt="User Avatar" width="50" height="50"
                               style={{borderRadius:"50%", objectFit:"cover"}} />
->>>>>>> 95f9e8802850a76545b7fa4b58e4296ea7b2bf79
                          </Link>
                     </div>
                </div>
